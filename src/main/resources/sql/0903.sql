13f5e66291297e1029c41f3cd81232d8e6f1504e
seungy07 <ksykim07@gmail.com>
2026-09-03 17:00:39 +0900

    일단 17:00

--- src/main/resources/sql/0903.sql
+++ src/main/resources/sql/0903.sql
@@ -1,7 +1,7 @@
 -- day05 / TestEntity sample, SQL 카멜표기법 대신에 _(언더바) 
 insert into test( name, descri , price , create_Date, update_Time)
-    VALUE('코카콜라' , '맛있는 탄산음료1', 1000, now(), now()),
-    ('사이다' , '맛있는 탄산음료2', 1600, now(), now()),
+    VALUE('코카콜라1' , '맛있는 탄산음료1', 1000, now(), now()),
+    ('사이다1' , '맛있는 탄산음료2', 1600, now(), now()),
     ('환타' , '맛있는 탄산음료3', 700, now(), now());
 
 
