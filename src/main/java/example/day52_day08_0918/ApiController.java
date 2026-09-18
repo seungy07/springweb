8dc92e480a43b8e60a99bb008e24e7271abd0f1d
seungy07 <ksykim07@gmail.com>
2026-09-18 10:16:40 +0900

    reactive web 의조ㄴ성 반영까지

---
+++ src/main/java/example/day52_day08_0918/ApiController.java
@@ -0,0 +1,8 @@
+package example.day52_day08_0918;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class ApiController {
+    
+}
---
+++ src/main/java/example/day52_day08_0918/ApiService.java
@@ -0,0 +1,8 @@
+package example.day52_day08_0918;
+
+import org.springframework.stereotype.Service;
+
+@Service 
+public class ApiService {
+    
+}
